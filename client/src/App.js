ddf2241d77e9f0091e93ca4c98cf0e1688a2312f
ReTims <renatatims@gmail.com>
2023-04-05 11:51:17 -0500

    Set-up openai dependency + NutriChat function to render a user form and send the input to openai and display the answer from chatGPT - basic structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,51 @@
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import { Configuration, OpenAIApi } from 'openai';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const configuration = new Configuration({
+  apiKey: API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function getResponse(userInput) {
+  const prompt = userInput.trim();
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: prompt,
+    temperature: 0.7,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+
+  return response.data.choices[0].text;
+}
+
+function NutriChat() {
+  const [input, setInput] = useState('');
+  const [response, setResponse] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const responseText = await getResponse(input);
+    setResponse(responseText);
+  };
+
   return (
-    <h1>nutri-gpt</h1>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Message:
+          <input type="text" value={input} onChange={(event) => setInput(event.target.value)} />
+        </label>
+        <button type="submit">Send</button>
+      </form>
+      {response && <p>{response}</p>}
+    </div>
   );
 }
 
-export default App;
+export default NutriChat;
