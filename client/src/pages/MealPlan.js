66f0528bc7f4eb465930e4538a57bca3962e69b4
ReTims <renatatims@gmail.com>
2023-04-12 15:34:16 -0500

    Created the following functions - handleEditTitle, handleSaveTitle and handleTitleChange + added state variables and mutation to edit and save the title of the Meal Plans

--- client/src/pages/MealPlan.js
+++ client/src/pages/MealPlan.js
@@ -1,15 +1,43 @@
-import React from "react";
-import { Card, IconButton } from "@mui/material";
+import React, { useState } from "react";
+import { Card, IconButton, TextField } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
 
 //use Query Hook
 import { useQuery } from "@apollo/client";
 import { QUERY_NUTRI_PLANS } from "../utils/queries";
 
+//use Mutation to update the title
+import { useMutation } from "@apollo/client";
+import { UPDATE_NUTRI_PLAN_TITLE } from "../utils/mutations";
+
 const SavedMealPlans = () => {
+  //Define state variables for editing the title
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState("");
+
   const { data } = useQuery(QUERY_NUTRI_PLANS);
   console.log(data);
   const nutriPlans = data?.nutriPlans || [];
+
+  const [updateTitle] = useMutation(UPDATE_NUTRI_PLAN_TITLE);
+
+  const handleEditTitle = (nutriPlanId) => {
+    setIsEditing(true);
+  };
+
+  const handleSaveTitle = async (nutriPlanId) => {
+    if (editedTitle.trim() !== "") {
+      await updateTitle({
+        variables: { nutriPlanId, title: editedTitle },
+        refetchQueries: [{ query: QUERY_NUTRI_PLANS }],
+      });
+      setIsEditing(false);
+    }
+  };
+
+  const handleTitleChange = (e) => {
+    setEditedTitle(e.target.value);
+  };
 
   return (
     <>
@@ -19,10 +47,26 @@ const SavedMealPlans = () => {
           variant="outlined"
           sx={{ m: "20px", borderColor: "#8C2E5A", borderWidth: "5px" }}
         >
-          <h2>{nutriPlan.title} {" "}
-          <IconButton>
-            <EditIcon />
-          </IconButton>
+          <h2>
+            {isEditing ? (
+              <TextField
+                value={editedTitle}
+                onChange={handleTitleChange}
+                label="Meal Plan Title"
+              />
+            ) : (
+              <>
+                {nutriPlan.title}{" "}
+                <IconButton onClick={() => handleEditTitle(nutriPlan._id)}>
+                  <EditIcon />
+                </IconButton>
+              </>
+            )}
+            {isEditing && (
+            <IconButton onClick={() => handleSaveTitle(nutriPlan._id)}>
+              Save
+            </IconButton>
+          )}
           </h2>
           {nutriPlan.meals.split("\n").map((meal, index) => {
             const [mealInfo, mealDetails] = meal.split(":");
